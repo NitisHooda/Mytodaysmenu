810f38054fa1bc599a59f317ba5210c0cb40c1a6
Nitish Hooda <nitish@whiite.co>
2017-08-26 11:59:21 +0530

    mongo db connection

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoUrl : 'mongodb://localhost:27017/mytodaysmenu'
+    mongoUrl : 'mongodb://Nitish:monitz@ec2-52-25-137-126.us-west-2.compute.amazonaws.com:27017/mytodaysmenu'
 }
